e87a0be1e1fd758f1c8b01782f70929e7152211e
Wladi Veras <hi@wladi.com.br>
2024-06-13 12:24:38 -0300

    chore: Add reactive features to Landing/Resource.vue

--- components/Landing/Resource.vue
+++ components/Landing/Resource.vue
@@ -1,4 +1,43 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const features = reactive([
+  {
+    title: "Fácil de usar",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when",
+    icon: "solar:star-outline",
+  },
+  {
+    title: "Confiável",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when",
+    icon: "carbon:security",
+  },
+  {
+    title: "Seguro",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when",
+    icon: "ic:outline-lock",
+  },
+  {
+    title: "Rápido",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when",
+    icon: "akar-icons:thunder",
+  },
+  {
+    title: "Custo-benefício",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when",
+    icon: "f7:money-dollar-circle",
+  },
+  {
+    title: "Escalável",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when",
+    icon: "mdi:finance",
+  },
+])
+</script>
 <template>
   <ULandingSection>
     <div>
@@ -11,18 +50,20 @@
         </h1>
       </div>
       <ULandingGrid class="mt-20">
-        <ULandingCard class="col-span-6 row-span-2 bg-pink-50">
+        <ULandingCard
+          class="col-span-6 row-span-2 bg-pink-50"
+          v-for="feature in features"
+          :key="feature.title"
+        >
           <template #title>
             <div class="flex items-center text-pink-600">
-              <UIcon name="solar:star-outline" size="22px" class="mr-5" />
-              <span class="font-semibold"> Fácil de usar </span>
+              <UIcon :name="feature.icon" size="22px" class="mr-5" />
+              <span class="font-semibold"> {{ feature.title }} </span>
             </div>
           </template>
           <template #description>
             <p class="text-[#9A7587] mt-5 text-sm">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since the
-              1500s, when
+              {{ feature.description }}
             </p>
           </template>
         </ULandingCard>
