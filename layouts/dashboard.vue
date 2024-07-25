b4fed093a1b178cf447ab28c0a8185c0318bd864
Felipe Barreto <felipebarreto148@gmail.com>
2024-07-24 21:54:46 -0300

    feat: change dashboard layout

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -5,15 +5,8 @@
     <DashboardHeader />
     <main class="flex flex-1 overflow-hidden">
       <DashboardSidebar />
-      <main class="flex-1 p-10 pb-5 overflow-auto">
-        <ULandingSection
-        :ui="{
-          wrapper: '',
-          container: 'relative gap-16 flex flex-col',
-        }"
-          >
-            <NuxtPage />
-        </ULandingSection>
+      <main class="flex-1 p-10 pb-5 overflow-auto flex flex-col relative gap-16">
+          <NuxtPage />
       </main>
     </main>
   </main>
