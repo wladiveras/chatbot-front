a07fb20d1fc21eb8b238499c10858696483d8b41
Wladi Veras <hi@wladi.com.br>
2024-08-04 12:03:10 -0300

    chore: Update npm dependencies and add Pixi.js and Vue3 Pixi packages

--- pages/Flows/[id].vue
+++ pages/Flows/[id].vue
@@ -1,65 +1,86 @@
-<script setup>
-import { VueFlow, useVueFlow } from "@vue-flow/core"
-import { initialEdges, initialNodes } from "@/utils/initial-elements.js"
+<script setup lang="ts">
+import { useEventListener } from "@vueuse/core"
 
-const route = useRoute()
-const sidebarStore = useSidebarStore()
-const flowsStore = useFlowsStore()
-const { isExpanded } = storeToRefs(sidebarStore)
-const { flow, edge, node } = storeToRefs(flowsStore)
+import type { SpriteInst, SpriteProps } from "vue3-pixi"
+import { useApplication, useScreen, useStage } from "vue3-pixi"
 
-await flowsStore.fetchFlow(route.params?.id)
+// todo: Pixi -->
+const eventName = ref("none")
 
-const {
-  onInit,
-  onNodeDragStop,
-  onConnect,
-  addEdges,
-  addNodes,
-  onNodeClick,
-  setCenter,
-} = useVueFlow()
-const nodes = ref(initialNodes)
-const edges = ref(initialEdges)
+const nodes = [
+  { x: 175, y: 75, scale: 1.2 },
+  { x: 655, y: 75 },
+  { x: 410, y: 325, rotation: Math.PI / 10 },
+  { x: 150, y: 465, scale: 0.8 },
+  { x: 685, y: 445, scale: { x: 0.8, y: 1.2 }, rotation: Math.PI },
+]
 
-onInit((vueFlowInstance) => {
-  vueFlowInstance.fitView()
-})
-
-onNodeDragStop(({ event, nodes, node }) => {
-  console.log("Node Drag Stop", { event, nodes, node })
-})
-
-onConnect((connection) => {
-  addEdges(connection)
-})
-
-onNodeClick(({ node }) => {
-  if (node.type !== "init") {
-    if (isExpanded.value) sidebarStore.toggleSize()
-    flowsStore.setSelectedNode(node)
-    setCenter(node.position.x, node.position.y, { duration: 200, zoom: 1 })
-  }
-})
-
-function addNewStep() {
-  sidebarStore.toggleSize()
-
-  addNodes({
-    id: "4",
-    data: { label: "Node 4" },
-    position: { x: 400, y: 300 },
-    class: "custom-node init",
-  })
+const eventHandler = (name, evt) => {
+  eventName.value = name
+  console.log(name, evt)
 }
 
-onUnmounted(() => {
-  console.log(isExpanded.value)
-  if (!isExpanded.value) {
-    addNewStep()
-  }
+function drawArc(e) {
+  e.beginFill("#4f455c")
+  e.arc(0, 0, 100, 0, Math.PI, false)
+}
+
+function onButtonDown() {}
+
+function onButtonUp() {}
+
+function onButtonOver() {}
+
+function onButtonOut() {}
+
+// Dragg
+
+const stage = useStage()
+
+// create a texture from an image path
+
+const sprites = ref<Partial<SpriteProps>[]>([])
+const target = ref<SpriteInst>()
+
+function onDragStart(this: SpriteInst) {
+  // store a reference to the data
+  // the reason for this is because of multitouch
+  // we want to track the movement of this particular touch
+  // this.data = event.data;
+  this.alpha = 0.5
+  target.value = this
+  stage.value.on("pointermove", onDragMove)
+}
+
+function onDragMove(event: any) {
+  if (target.value) Object.assign(sprites.value[target.value!.zIndex], event.global)
+}
+
+function onDragEnd() {
+  if (!target.value) return
+  stage.value.off("pointermove", onDragMove)
+  target.value.alpha = 1
+  target.value = undefined
+}
+
+onReady((app) => {
+  sprites.value = Array(10)
+    .fill(undefined)
+    .map(() => ({
+      x: Math.floor(Math.random() * app.screen.width),
+      y: Math.floor(Math.random() * app.screen.height),
+      onPointerdown: onDragStart,
+      nodes,
+      scale: 3,
+      anchor: 0.5,
+    }))
+  // Enable interactivity!
+  app.stage.eventMode = "static"
+  app.stage.hitArea = app.screen
 })
 
+useEventListener(stage, "pointerup", onDragEnd)
+useEventListener(stage, "pointerupoutside", onDragEnd)
 definePageMeta({
   layout: "flows",
 })
@@ -67,7 +88,7 @@ definePageMeta({
 
 <template>
   <main class="flex gap-5 w-full h-full">
-    <aside v-if="!isExpanded" class="w-full max-w-72 h-full border-r border-[#E5E5E5]">
+    <aside v-if="true" class="w-full max-w-72 h-full border-r border-[#E5E5E5]">
       <header
         class="border-b border-[#E5E5E5] p-5 flex items-center justify-between text-gray-500 font-semibold text-base"
       >
@@ -75,11 +96,7 @@ definePageMeta({
           <UIcon name="material-symbols:inventory-2-outline" class="size-5" />
           <p>Conteúdo</p>
         </section>
-        <UIcon
-          class="size-6 cursor-pointer"
-          name="material-symbols:close"
-          @click="sidebarStore.toggleSize()"
-        />
+        <UIcon class="size-6 cursor-pointer" name="material-symbols:close" @click="" />
       </header>
       <FlowsOptions />
     </aside>
@@ -88,26 +105,20 @@ definePageMeta({
         class="absolute right-5 z-50"
         icon="material-symbols:add"
         size="xl"
-        @click="addNewStep"
+        @click=""
       />
-      <ClientOnly>
-        <VueFlow
-          :nodes="nodes"
-          :edges="edges"
-          :default-viewport="{ zoom: 0.5 }"
-          :min-zoom="0.5"
-          :max-zoom="3"
-          fit-view-on-init
-        >
-          <template #node-init="props">
-            <NodeInit :node="props" />
-          </template>
-
-          <template #node-content="props">
-            <NodeContent :node="props" />
-          </template>
-        </VueFlow>
-      </ClientOnly>
+      <Application :width="1630" :height="835">
+        <text :anchor="0.5" :x="120" :y="120" :style="{ fill: 'white' }">
+          Hello NuxtJS World
+        </text>
+        <graphics
+          @render="drawArc"
+          v-for="node in nodes"
+          :key="node.x"
+          :x="node.x"
+          :y="node.y"
+        />
+      </Application>
     </section>
   </main>
 </template>
