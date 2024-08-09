5d72c0fff2b9bacf54020f9f4dcef88e6ac2ada6
Wladi Veras <hi@wladi.com.br>
2024-08-09 05:32:51 -0300

    chore: Add Access-Control headers to useMakeRequests.ts

--- composables/useMakeRequests.ts
+++ composables/useMakeRequests.ts
@@ -18,6 +18,9 @@ export default function () {
         const headers = {
           Accept: "application/json",
           "Cache-Control": "no-cache",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
           ...options?.headers,
         };
 
